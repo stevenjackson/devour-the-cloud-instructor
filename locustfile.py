9f9409f7828ceb53fc36d6db8a6878e988534dac
Steve Jackson <steve@testdouble.com>
2018-01-06 15:32:08 -0500

    Update locustfile to finding baseline start

--- locustfile.py
+++ locustfile.py
@@ -2,6 +2,7 @@ from locust import HttpLocust, TaskSet, task
 from http.cookies import SimpleCookie
 from bs4 import BeautifulSoup
 import json
+import time
 from faker import Faker
 
 fake = Faker()
@@ -9,6 +10,7 @@ fake = Faker()
 HOST = "http://localhost:8000"
 MIN_WAIT = 5000
 MAX_WAIT = 9000
+USER_SPEED = 1 #higher is faster
 
 class Helpers:
     def parse_csrf(self, string):
@@ -16,14 +18,18 @@ class Helpers:
         if tag is not None:
             return tag["value"]
 
-    def post_ajax(self, url, params):
+    def post_ajax(self, url, params, **kwargs):
         csrf_token = self.client.cookies.get("csrftoken", "")
-        response =  self.client.post(url, params, headers={"X-CSRFToken": csrf_token, "X-Requested-With": "XMLHttpRequest"})
+        kwargs.update(headers={"X-CSRFToken": csrf_token, "X-Requested-With": "XMLHttpRequest"})
+        response =  self.client.post(url, params, **kwargs)
         #These ajax responses don't have domains in the cookie so CookieJar is throwing them away
         self.store_cookies(response)
         return response
 
     def store_cookies(self, response):
+        if not "Set-Cookie" in response.headers:
+            return
+
         cookies = SimpleCookie(response.headers["Set-Cookie"])
         for key, item in cookies.items():
             self.client.cookies.set(item.key, item.value)
@@ -37,17 +43,37 @@ class Helpers:
         variants = soup.select_one("#variant-picker")["data-variant-picker-data"]
         return json.loads(variants)["variants"]
 
-class VisitorBehavior(TaskSet, Helpers):
-    @task(100)
-    def buy(self):
-        self.client.get("/")
-        response = self.client.get("/products/category/apparel-1/")
-        response = self.client.get("/products/codemash-1/")
-        response = self.post_ajax("/products/codemash-1/add/", {"quantity":1, "variant":1 })
-        response = self.client.get("/checkout/")
+    def generate_email(self):
+        return "{}_{:f}@example.com".format(fake.user_name(), time.time())
+
+    def simulate_user_form_fill_time(self, task_time):
+        time.sleep(task_time / USER_SPEED)
+
+class SharedTasks(Helpers):
+    def register_user(self):
+        response = self.client.get("/account/signup/")
         csrf_token = self.parse_csrf(response.text)
+        response = self.client.post("/account/signup/", {
+            "csrfmiddlewaretoken":csrf_token,
+            "email":self.generate_email(),
+            "password":fake.password()
+        })
+
+    def find_hoodie(self):
+        self.client.get("/", name="1 home")
+        self.client.get("/products/category/apparel-1/", name="2 /products/category/apparel-1/")
+        self.client.get("/products/codemash-1/", name="3 /products/codemash-1/")
+
+    def add_to_cart(self):
+        self.post_ajax("/products/codemash-1/add/", {"quantity":1, "variant":1 }, name="4 products/codemash-1/add/")
+
+    def checkout(self):
+        response = self.client.get("/checkout/", name="5 /checkout/")
+        csrf_token = self.parse_csrf(response.text)
+        self.simulate_user_form_fill_time(30)
         response = self.client.post("/checkout/shipping-address/", {
             "csrfmiddlewaretoken":csrf_token,
+            "address":"new_address",
             "email":fake.email(),
             "phone":"1234567890",
             "first_name": fake.first_name(),
@@ -59,26 +85,73 @@ class VisitorBehavior(TaskSet, Helpers):
             "country_area": "OH",
             "postal_code": "44111",
             "country": "US",
-        })
+        }, name="6 /checkout/shipping-address/")
         csrf_token = self.parse_csrf(response.text)
+        self.simulate_user_form_fill_time(5)
         response = self.client.post("/checkout/shipping-method/", {
             "csrfmiddlewaretoken":csrf_token,
             "method": "1"
-        })
+        }, name="7 /checkout/shipping-method/")
         csrf_token = self.parse_csrf(response.text)
         response = self.client.post("/checkout/summary/", {
             "csrfmiddlewaretoken":csrf_token,
             "address": "shipping_address"
-        })
+        }, name="8 /checkout/summary/")
         csrf_token = self.parse_csrf(response.text)
         response = self.client.post(response.url, {
             "csrfmiddlewaretoken":csrf_token,
             "method": "default"
-        }, name="/order/[order-id]/payment")
+        }, name="9 /order/[order-id]/payment")
+
+class VisitorBehavior(TaskSet, SharedTasks):
+    @task(3)
+    def just_visiting(self):
+        self.client.get("/")
+
+    @task(47)
+    def search(self):
+        self.find_hoodie()
+
+    @task(25)
+    def bail_on_cart(self):
+        self.find_hoodie()
+        self.add_to_cart()
+
+    @task(25)
+    def buy(self):
+        self.find_hoodie()
+        self.add_to_cart()
+        self.checkout()
+
+class ReturningUserBehavior(TaskSet, SharedTasks):
+    def on_start(self):
+        self.register_user()
+
+    @task(20)
+    def search(self):
+        self.find_hoodie()
+
+    @task(5)
+    def bail_on_cart(self):
+        self.find_hoodie()
+        self.add_to_cart()
+
+    @task(75)
+    def buy(self):
+        self.find_hoodie()
+        self.add_to_cart()
+        self.checkout()
 
 class Visitor(HttpLocust):
-    weight = 5
+    weight = 80
     task_set = VisitorBehavior
+    host = HOST
+    min_wait = MIN_WAIT
+    max_wait = MAX_WAIT
+
+class ReturningUser(HttpLocust):
+    weight = 20
+    task_set = ReturningUserBehavior
     host = HOST
     min_wait = MIN_WAIT
     max_wait = MAX_WAIT
